a4582640cbfd758e7930ddbe27f77f9d7405ed0a
Salman A. <salmana.ar@gmail.com>
2026-04-19 18:35:01 +0530

    chore: update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import favicon from '$lib/assets/favicon.png';
+	import faviconApple from '$lib/assets/favicon-apple.png';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} type="image/png" />
+	<link rel="apple-touch-icon" href={faviconApple} />
+</svelte:head>
 
 {@render children()}
